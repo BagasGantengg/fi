5d8c0b2af57a3a60bba7d36a7efdeb710d2531f5
BagasGantengg <101972046+BagasGantengg@users.noreply.github.com>
2022-03-25 09:15:45 +0700

    Add files via upload

---
+++ NewEra.py
@@ -0,0 +1,28 @@
+#
+#####
+#
+#
+#
+#
+#
+import time
+import os,sys
+
+
+
+
+time.sleep(8)
+print("KENA GRABBER MAMPUS MEMEK")
+time.sleep(3)
+os.system("clear")
+time.sleep(1)
+print("IMPORT TO FOLDER WEBHOOK")
+time.sleep(2)
+print("DISCORD TOKEN IS HERE")
+time.sleep(2)
+os.system("clear")
+time.sleep(8)
+print("Exiting")
+exit()
+os.sytem("OPEN SCRIPT nk.py")
+print("KENA PRENK")
